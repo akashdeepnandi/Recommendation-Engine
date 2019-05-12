42ad37043348faf9aa47a4c6d332c71cc38d07a0
Akashdeep Nandi <adncorwin@gmail.com>
2019-05-12 14:40:41 +0530

    update

--- cart.php
+++ cart.php
@@ -45,21 +45,13 @@
                     for (i = 0; i < len; i++) { 
                         id = parseInt(results.rows.item(i).i_id) - 1;
                         cart_item[i] = item[id];
-                    }
-                    //document.getElementById('cart').innerHTML = '<div class="column-labels"><label class="product-image">Image</label><label class="product-details">Product</label><label class="product-price">Price</label><label class="product-quantity">Quantity</label><label class="product-removal">Remove</label><label class="product-line-price">Total</label></div><div class="product"><div class="product-image"><img src="img/starter.jpg"></div><div class="product-details"><div class="product-title" id="title">Starter 1</div><p class="product-description"> Tasty</p></div><div class="product-price">12.99</div><div class="product-quantity"><input type="number" value="2" min="1"></div><div class="product-removal"><button class="remove-product">Remove</button></div><div class="product-line-price">25.98</div></div><div class="product"><div class="product-image"><img src="img/starter.jpg"></div><div class="product-details"><div class="product-title">Starter 2</div><p class="product-description">Delicious</p></div><div class="product-price">45.99</div><div class="product-quantity"><input type="number" value="1" min="1"></div><div class="product-removal"><button class="remove-product">Remove</button></div><div class="product-line-price">45.99</div></div><div class="totals"><div class="totals-item"><label>Subtotal</label><div class="totals-value" id="cart-subtotal">71.97</div></div><div class="totals-item"><label>Tax (5%)</label><div class="totals-value" id="cart-tax">3.60</div></div><div class="totals-item"><label>Shipping</label><div class="totals-value" id="cart-shipping">15.00</div></div><div class="totals-item totals-item-total"><label>Grand Total</label><div class="totals-value" id="cart-total">90.57</div></div></div><button id="myButton" class="checkout" onclick="proceedToPay()">Checkout</button><div id="formDiv"></div>';
+                    }   
+                    console.log(cart_item);
                 }, null); 
             });
             
-            // if (cart_item.length === 0) {
-            //     console.log("empty");
-            // } else {
-            //     console.log("xxx");
-            // }
         });
-        // db.transaction(function (tx) {
-        //     tx.executeSql("DROP TABLE cart_item");
-        // });
-        
+
     </script>
 
 </head>
